615e5e96daf8dd0a7c77d122d8def4fa7407942e
izayl <izayl@163.com>
2021-01-27 03:21:08 +0800

    feat: use lerna

---
+++ packages/eslint-config-basic/index.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    browser: true,
+  },
+  extends: ['standard', 'plugin:import/errors', 'plugin:import/warnings'],
+  rules: {
+    // import
+    'import/order': 'error',
+    'import/first': 'error',
+    'import/no-mutable-exports': 'error',
+    'import/no-unresolved': 'off',
+    'import/no-absolute-path': 'off',
+
+    // common
+    'no-unused-vars': 'warn',
+    'space-before-function-paren': ['error', 'never'],
+    'comma-dangle': ['error', 'always-multiline'],
+  },
+}
