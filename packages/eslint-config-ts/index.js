398163ae6f24d77385fd2843e271cb012dd2f486
izayl <izayl@163.com>
2021-01-27 21:08:25 +0800

    feat: create eslint-config-ts

---
+++ packages/eslint-config-ts/index.js
@@ -0,0 +1,31 @@
+module.exports = {
+  extends: [
+    '@izayl/eslint-config-basic',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  rules: {
+    '@typescript-eslint/indent': [
+      'error',
+      2,
+      {
+        SwitchCase: 1,
+        ignoredNodes: ['TSTypeParameterInstantiation'],
+      },
+    ],
+    '@typescript-eslint/member-delimiter-style': ['error', {
+      multiline: {
+        delimiter: 'none', // 'none' or 'semi' or 'comma'
+        requireLast: true,
+      },
+      singleline: {
+        delimiter: 'semi', // 'semi' or 'comma'
+        requireLast: false,
+      },
+    }],
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'no-useless-constructor': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+  },
+}
